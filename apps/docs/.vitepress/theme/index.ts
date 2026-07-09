9cbd0564605e257d261b3dd631c991ff97582737
LiJianying <lijy91@foxmail.com>
2026-07-09 22:49:05 +0800

    chore(docs): remove TypeBuddy advertisement component

--- apps/docs/.vitepress/theme/index.ts
+++ apps/docs/.vitepress/theme/index.ts
@@ -2,14 +2,7 @@ import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import 'virtual:group-icons.css'
 import './styles.css'
-import TypeBuddyAd from './components/TypeBuddyAd.vue'
 
 export default {
   extends: DefaultTheme,
-
-  Layout() {
-    return h(DefaultTheme.Layout, null, {
-      'aside-ads-before': () => h(TypeBuddyAd),
-    })
-  },
 }
